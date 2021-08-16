9105379014f6636a1e9e9d4f0c3ec770522ca35b
Andrej-cyber <andvit94@gmail.com>
2021-08-16 16:00:07 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import UserSearch from './refs/UserSearch';
+
+const App = () => {
+  return ( <div>
+   <UserSearch />
+  </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
